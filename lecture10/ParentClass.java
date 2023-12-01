1188def72689f94823e5df8fc7281f45d0d48076
Avramovskak <147843147+Avramovskak@users.noreply.github.com>
2023-12-01 18:48:19 +0100

    Create ParentClass.java

---
+++ lecture10/ParentClass.java
@@ -0,0 +1,21 @@
+public class ParentClass {
+
+	private String Product;
+	private int ProductId;
+
+	public String getProduct() {
+		return Product;
+	}
+
+	public void setProduct(String product) {
+		this.Product = product;
+	}
+
+	public int getProductId() {
+		return ProductId;
+	}
+
+	public void setProductId(int Id) {
+		this.ProductId = Id;
+	}
+}
